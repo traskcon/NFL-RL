8423bcb55ad811072cbadeaed78f7cdab011ad97
Connor Trask <ctrask@ida.org>
2024-09-17 16:34:46 -0400

    Added era explanations

--- ep_model.py
+++ ep_model.py
@@ -2,14 +2,21 @@ import xgboost
 import numpy as np
 
 model = xgboost.Booster()
-model.load_model('epa.model')
+model.load_model('C:/Users/ctrask/Documents/NFL-RL/epa.model')
 
 # NFLFastR's Expected Points (EP) model is an XGBoost Classifier
 # It takes 9 variables as inputs and uses an encoding to map the string variables (and season) to boolean columns
 # As a result, the actual XGBoost classifier model has 18 features
 # ["half_seconds_remaining", "yardline_100", "home", "retractable", "dome", "outdoors", "ydstogo", "era0", "era1", "era2", "era3", "era4", "down1", "down2", "down3", "down4",
 #   "posteam_timeouts_remaining", "defteam_timeouts_remaining"]
-data = np.array([[1800, 90, 1, 0, 0, 1, 10, 0, 0, 0, 0, 1, 1, 0, 0, 0, 3, 3]], dtype=object)
+
+# Eras correspond to certain season ranges:
+#   era0: Seasons 2001 and earlier
+#   era1: Seasons (2001, 2005]
+#   era2: Seasons (2005, 2013]
+#   era3: Seasons (2013, 2017]
+#   era4: Seasons after 2017
+data = np.array([[1800, 90, 1, 0, 0, 0, 10, 0, 0, 0, 0, 1, 1, 0, 0, 0, 3, 3]], dtype=object)
 y = np.array([0.507])
 
 dtest = xgboost.DMatrix(data, y)
