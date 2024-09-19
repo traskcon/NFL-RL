2cb69d2b4444625dc6a053b534a306c938320e84
Connor Trask <traskcon@msu.edu>
2024-09-18 20:46:08 -0400

    Changed agent positions to np arrays

--- environments/envs/multiagent_environment.py
+++ environments/envs/multiagent_environment.py
@@ -12,39 +12,38 @@ class MultiEnvironment(ParallelEnv):
         '''Initial WR-DB Coverage Battle environment
         Based on PettingZoo prison escape tutorial: https://pettingzoo.farama.org/tutorials/custom_environment/2-environment-logic/
         '''
-        #TODO: Convert locations into np arrays, like in single agent environment
-        self.target_y = None
-        self.target_x = None
-        self.db_y = None
-        self.db_x = None
-        self.wr_y = None
-        self.wr_x = None
+        self.width = 7
+        self.length = 7
+        self.target_location = np.array([None, None])
+        self.db_location = np.array([None, None])
+        self.wr_location = np.array([None, None])
         self.timestep = None
         self.possible_agents = ["WR", "DB"]
+
+
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
 
     def reset(self, seed=None, options=None):
         '''Re-initialize the environment'''
         self.agents = copy(self.possible_agents)
         self.timestep = 0
 
-        self.wr_x = 0
-        self.wr_y = 0
+        self.wr_location = np.array([0, 0])
 
-        self.db_x = 6
-        self.db_y = 6
+        self.db_location = np.array([6, 6])
 
-        self.target_x = np.random.randint(2,5)
-        self.target_y = np.random.randint(2,5)
+        self.target_location = np.random.randint(2,5,2)
 
-        observation = (
-            self.wr_x + 7 * self.wr_y,
-            self.db_x + 7 * self.db_y,
-            self.target_x + 7 * self.target_y,
-        )
-        observations = {
-            "WR": {"WR": observation, "action_mask": [0,1,1,0]},
-            "DB": {"DB": observation, "action_mask": [1,0,0,1]},
-        }
+        observations = {a:(
+            np.dot([1,7], self.wr_location),
+            np.dot([1,7], self.db_location),
+            np.dot([1,7], self.target_location),
+        ) for a in self.agents}
 
         # Get dummy infos. Necessary for proper parallel_to_aec conversion
         infos = {a: {} for a in self.agents}
@@ -57,62 +56,25 @@ class MultiEnvironment(ParallelEnv):
         wr_action = actions["WR"]
         db_action = actions["DB"]
 
-        if wr_action == 0 and self.wr_x > 0:
-            self.wr_x -= 1
-        elif wr_action == 1 and self.wr_x < 6:
-            self.wr_x += 1
-        elif wr_action == 2 and self.wr_y > 0:
-            self.wr_y -= 1
-        elif wr_action == 3 and self.wr_y < 6:
-            self.wr_y += 1
+        # Map the action (element of {0,1,2,3}) to the direction we walk in
+        wr_direction = self._action_to_direction[wr_action]
+        # We use `np.clip` to make sure we don't leave the grid
+        self.wr_location = np.clip(
+            self.wr_location + wr_direction, [0, 0], [self.width - 1, self.length - 1]
+        )
 
-        if db_action == 0 and self.db_x > 0:
-            self.db_x -= 1
-        elif db_action == 1 and self.db_x < 6:
-            self.db_x += 1
-        elif db_action == 2 and self.db_y > 0:
-            self.db_y -= 1
-        elif db_action == 3 and self.db_y < 6:
-            self.db_y += 1
-
-        # Generate action masks
-        wr_action_mask = np.ones(4, dtype=np.int8)
-        if self.wr_x == 0:
-            wr_action_mask[0] = 0  # Block left movement
-        elif self.wr_x == 6:
-            wr_action_mask[1] = 0  # Block right movement
-        if self.wr_y == 0:
-            wr_action_mask[2] = 0  # Block down movement
-        elif self.wr_y == 6:
-            wr_action_mask[3] = 0  # Block up movement
-
-        db_action_mask = np.ones(4, dtype=np.int8)
-        if self.db_x == 0:
-            db_action_mask[0] = 0
-        elif self.db_x == 6:
-            db_action_mask[1] = 0
-        if self.db_y == 0:
-            db_action_mask[2] = 0
-        elif self.db_y == 6:
-            db_action_mask[3] = 0
-
-        # Action mask to prevent db from going over target cell
-        if self.db_x - 1 == self.target_x:
-            db_action_mask[0] = 0
-        elif self.db_x + 1 == self.target_x:
-            db_action_mask[1] = 0
-        if self.db_y - 1 == self.target_y:
-            db_action_mask[2] = 0
-        elif self.db_y + 1 == self.target_y:
-            db_action_mask[3] = 0
+        db_direction = self._action_to_direction[db_action]
+        self.db_location = np.clip(
+            self.db_location + db_direction, [0, 0], [self.width - 1, self.length - 1]
+        )
 
         # Check termination conditions
         terminations = {a: False for a in self.agents}
         rewards = {a: 0 for a in self.agents}
-        if self.wr_x == self.target_x and self.wr_y == self.target_y:
+        if np.array_equal(self.wr_location, self.target_location):
             rewards = {"WR": 1, "DB": -1}
             terminations = {a: True for a in self.agents}
-        elif self.wr_x == self.db_x and self.wr_y == self.db_y:
+        elif np.array_equal(self.wr_location, self.db_location):
             rewards = {"WR": -1, "DB": 1}
             terminations = {a: True for a in self.agents}
         
@@ -124,15 +86,11 @@ class MultiEnvironment(ParallelEnv):
         self.timestep += 1
 
         # Get observations
-        observation = (
-            self.wr_x + 7 * self.wr_y,
-            self.db_x + 7 * self.db_y,
-            self.target_x + 7 * self.target_y,
-        )
-        observations = {
-            "WR": {"observation": observation, "action_mask": wr_action_mask,},
-            "DB": {"observation": observation, "action_mask": db_action_mask,},
-        }
+        observations = {a: (
+            np.dot([1,7], self.wr_location),
+            np.dot([1,7], self.db_location),
+            np.dot([1,7], self.target_location),
+        ) for a in self.agents}
 
         # Get dummy infos
         infos = {a: {} for a in self.agents}
@@ -145,9 +103,9 @@ class MultiEnvironment(ParallelEnv):
     def render(self):
         '''Render the environment'''
         grid = np.full((7,7), ' ')
-        grid[self.wr_y, self.wr_x] = "WR"
-        grid[self.db_y, self.db_x] = "DB"
-        grid[self.target_y, self.target_x] = "T"
+        grid[self.wr_location[1], self.wr_location[0]] = "WR"
+        grid[self.db_location[1], self.db_location[0]] = "DB"
+        grid[self.target_location[1], self.target_location[0]] = "T"
         print(f"{grid} \n")
 
     #Define observation space
