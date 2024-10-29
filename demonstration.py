08fe1fe3cab74fcd347f5cfcb3b76d55ce88e9f7
Connor Trask <ctrask@ida.org>
2024-10-29 14:22:57 -0400

    Demonstrated Multiroute convergence!

--- demonstration.py
+++ demonstration.py
@@ -1,9 +1,10 @@
 # Script to demonstrate trained agents operating in the environment
 from environments.envs import multiagent_environment
+from environments import db_battle
 import numpy as np
 import policy
 
-scenario = multiagent_environment.Scenario()
+scenario = db_battle.Scenario()
 env = multiagent_environment.MultiEnvironment(scenario=scenario, max_cycles=100, render_mode="human")
 observations, infos = env.reset()
 learner = policy.Policy(env, observations)
@@ -15,7 +16,7 @@ while env.world.agents:
     if np.random.random() <= epsilon:
         actions = {agent.name: env.action_space(agent).sample() for agent in env.world.agents}
     else:
-        actions = {agent.name: learner.choose_action(agent, observations[agent.name], method="heuristic")
+        actions = {agent.name: learner.choose_action(agent, observations[agent.name], method="DQN")
                for agent in env.world.agents}
     new_observations, rewards, terminations, truncations, infos = env.step(actions)
     env.render()
