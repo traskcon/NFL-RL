e08a03c1d5b2d23c2c8fe46aee275ce1ac9666f0
Connor Trask <traskcon@msu.edu>
2024-11-07 19:38:20 -0500

    Reset demonstration to DB-Battle

--- demonstration.py
+++ demonstration.py
@@ -4,21 +4,21 @@ from environments import db_battle, all_22
 import numpy as np
 import policy
 
-scenario = all_22.Scenario()
-env = multiagent_environment.MultiEnvironment(scenario=scenario, max_cycles=100, render_mode="human", roster="FullTeam-Roster.csv")
+scenario = db_battle.Scenario()
+env = multiagent_environment.MultiEnvironment(scenario=scenario, max_cycles=100, render_mode="human", roster="DB-Battle-Roster.csv")
 observations, infos = env.reset()
-#learner = policy.Policy(env, observations)
-#learner.load_models("-MK3_MR")
+learner = policy.Policy(env, observations)
+learner.load_models("-MK3_MR")
 
 epsilon = 0.01
 
 while env.world.agents:
-    '''if np.random.random() <= epsilon:
+    if np.random.random() <= epsilon:
         actions = {agent.name: env.action_space(agent).sample() for agent in env.world.agents}
     else:
         actions = {agent.name: learner.choose_action(agent, observations[agent.name], method="DQN")
                for agent in env.world.agents}
-    new_observations, rewards, terminations, truncations, infos = env.step(actions)'''
+    new_observations, rewards, terminations, truncations, infos = env.step(actions)
     env.render()
-    #observations = new_observations
+    observations = new_observations
 env.close()
