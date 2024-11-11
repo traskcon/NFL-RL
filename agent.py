25b9012e7049d8325fbf91f87e03e2bc301dd6b8
Connor Trask <traskcon@msu.edu>
2024-11-11 10:24:20 -0500

    Removed extra libraries

--- agent.py
+++ agent.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-
-import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
-from matplotlib.patches import Patch
-from tqdm import tqdm
 
 import gymnasium as gym
 import environments
